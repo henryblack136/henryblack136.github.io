e486917e678ab2a4d3e90b9fe961159fa8e42633
henryblack136 <blackburnhenry26@gmail.com>
2025-01-07 21:18:47 +0000

    Added a lot.

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from . import util
+import markdown2
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def display(request, page):
+    x = util.get_entry(page)
+    if x == None:
+        return render(request, "encyclopedia/error.html")
+    else:
+        return render(request, "encyclopedia/entries.html", {
+            "page": page, "content": markdown2.markdown(x)
+        })
 
